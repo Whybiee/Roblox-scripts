a10d2d516fda4624b1baca82fe91ac46633b266f
Whybiee <98648167+Whybiee@users.noreply.github.com>
2022-02-19 20:38:56 +0000

    Update and rename Loader to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 8357510970 then do
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Whybiee/Roblox-scripts/main/Anime%20Punching%20Simulator.lua"))()
 end
+if game.PlaceId == 8649665676 then do
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Whybiee/Roblox-scripts/main/Anime%20Punching%20Simulator.lua"))()
+end
