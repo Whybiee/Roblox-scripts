88ca82927a834de248f9c3455c273b3ae1ec6504
Whybiee <98648167+Whybiee@users.noreply.github.com>
2022-02-21 12:54:46 +0000

    Update Anime Punching SimulatorNewWorld.lua

--- Anime Punching SimulatorNewWorld.lua
+++ Anime Punching SimulatorNewWorld.lua
@@ -45,3 +45,22 @@ c:Button("Teleport To Normal",function()
 end)
 
 c:DestroyGui()
+
+local GC = getconnections or get_signal_cons
+if GC then
+    for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
+        if v["Disable"] then
+            v["Disable"](v)
+        elseif v["Disconnect"] then
+            v["Disconnect"](v)
+        end
+    end
+else
+    -- use alt method since exploit doesnt support getconnections
+    local vu = game:GetService("VirtualUser")
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        wait(1)
+        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    end)
+end
